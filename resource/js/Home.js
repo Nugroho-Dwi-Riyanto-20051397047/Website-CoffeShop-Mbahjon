2e79de5cd1384ca983cfe36996898e9cb0e8a6f5
Nugroho Dwi Riyanto (20051397047) <72641363+Nugroho-Dwi-Riyanto-20051397047@users.noreply.github.com>
2021-12-27 12:47:07 +0700

    Create Home.js

---
+++ resource/js/Home.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.kopi1').src = anything;
+}
+
+function changeCricleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
